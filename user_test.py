8c775588eba434c8883883751a0392f5584ff861
dkibogy <jepkemboikibogy5@gmail.com>
2020-06-27 22:08:20 +0300

    run first test

--- user_test.py
+++ user_test.py
@@ -6,4 +6,14 @@ class TestUser(unittest.TestCase):
     test class that defines the test cases for the user behaviours,
     test case class helps in creating test cases
     """
-    
+    def setUp(self):
+
+        pass
+
+    def test_init(self):
+        """
+        test case to test if objects are initialised properly
+        """
+        pass 
+if __name__ == '__main__':
+    unittest.main()
