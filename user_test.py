406322ff6f9dcea55119287f56d181cc001e5002
dkibogy <jepkemboikibogy5@gmail.com>
2020-06-27 21:28:29 +0300

    create subclass called testuser that inherits from unitest.testcase

--- user_test.py
+++ user_test.py
@@ -1,2 +1,9 @@
 import unittest # Importing the unittest module
 from user import User # importing the user class
+
+class TestUser(unittest.TestCase):
+    """
+    test class that defines the test cases for the user behaviours,
+    test case class helps in creating test cases
+    """
+    
