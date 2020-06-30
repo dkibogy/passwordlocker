7ac9c838d4cfb2a16b8a50e9617a7a3c4d72e476
dkibogy <jepkemboikibogy5@gmail.com>
2020-06-30 09:50:53 +0300

    create behaviours for the new user to login and create their account

--- run.py
+++ run.py
@@ -11,4 +11,50 @@ def save_user(user):
 def del_user(user):
     user.delete_user()
 
+def main():
+    print("Hello, Welcome to password Locker")
+    print('\n')
+    print("use these short codes: nu - new user,lg - login, ex - exit the user ")
+    short_code = input().lower()
+    if short_code == 'nu':
+        print("create a new username")
+        create_user_name= input()
+
+        print("create a new password")
+        create_user_password= input()
+
+        print("confirm password")
+        confirm_password = input()
+
+        while confirm_password != create_user_password:
+            print("wrong password")
+            print("try again")
+            create_user_password= input()
+            print("confirm")
+            confirm_password = input()
+        else:
+            print(f"account created successful {create_user_name}!")
+            print("login")
+            typed_username = input()
+            print("password")
+            typed_password = input()
+        while typed_username != create_user_name or typed_password != create_user_password:
+            print("invalid password or username, try again")
+            typed_username = input()
+            print("password")
+            typed_password = input()
+            
+
+
+if __name__ == '__main__':
+
+    main()
+
+            
+
+
+
+
+
+
 
